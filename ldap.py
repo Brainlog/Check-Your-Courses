48fe86df201eab059e5fdc7956af31b07e967f03
Srijan1972 <srijang@live.com>
2024-10-29 19:59:07 +0530

    Formatting

--- ldap.py
+++ ldap.py
@@ -2,12 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import json
+import warnings
+warnings.filterwarnings("ignore")
 
 # Send an HTTP GET request to the website
 url = 'http://ldapweb.iitd.ac.in/LDAP/courses/'
-response = requests.get(url,verify=False)
+response = requests.get(url, verify=False)
 datadict = {}
-fileptr = open('data.json', 'w')
 
 # Check if the request was successful
 if response.status_code == 200:
@@ -17,14 +18,12 @@ if response.status_code == 200:
     # Extract data using BeautifulSoup
 
     links = soup.find_all('a', href=lambda href: href and href.endswith('.shtml'))
-    # data = soup.find('a href = ')
     
-    # Print the extracted data
+    # Format and dump the extracted data
     for i in range(0, len(links)):
-        print(i)
         temp = (links[i]['href'])
         temp = url + temp
-        response = requests.get(temp,verify=False)
+        response = requests.get(temp, verify=False)
         soup = BeautifulSoup(response.text, 'html.parser')
         course = soup.find_all('h2')[1].text
         data = soup.find_all('td', align = 'LEFT')
@@ -33,6 +32,8 @@ if response.status_code == 200:
                 datadict[data[j].text] = [course]
             else:
                 datadict[data[j].text].append(course)
+    fileptr = open('data.json', 'w')
     json.dump(datadict, fileptr)
+    print(f"Retrieved data of {len(links)} courses and {len(datadict)} people")
 else:
     print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
--- run.py
+++ run.py
@@ -1,65 +1,46 @@
 import json
-
 file_path = "data.json"  
 
 with open(file_path, 'r') as json_file:
     data = json.load(json_file)
     
 user_input = input("Kerbores: ")
-data1 = data[user_input]
-a=1
-b=1
-c=1
-d=1
-e=1
-f=1
-s = [set({}), set({}), set({}), set({}), set({}), set({})]
 
-for i in range(0, len(data1)):
-    if(data1[i][0:4] == '2102'):
-        if(a==1):
-            print("2102 4th SEM")
-            a=0
-        print(data1[i][5:])
-        s[0].add(data1[i][5:])
-    if(data1[i][0:4] == '2201'):  
-        if(b==1):
-            print("\n2201 5th SEM")
-            b=0      
-        print(data1[i][5:])
-        s[1].add(data1[i][5:])
-    if(data1[i][0:4] == '2202'):
-        if(c==1):
-            print("\n2202 6th SEM")
-            c=0
-        print(data1[i][5:])
-        s[2].add(data1[i][5:])
-    if(data1[i][0:4] == '2301'):
-        if(d==1):
-            print("\n2301 7th SEM")
-            d=0
-        print(data1[i][5:])
-        s[3].add(data1[i][5:])
-    if(data1[i][0:4] == '2302'):
-        if(e==1):
-            print("\n2302 8th SEM")
-            e=0
-        print(data1[i][5:])
-        s[4].add(data1[i][5:])
-    if(data1[i][0:4] == '2401'):
-        if(f==1):
-            print("\n2401 9th SEM")
-            f=0
-        print(data1[i][5:])
-        s[5].add(data1[i][5:])
-        
+courses = data[user_input]
+sems = set()
+done = {}
 
-set_fail = set({})
+for course in courses:
+    if not course[0:4].isdigit():
+        try:
+            done['2101'].add(course[0:6])
+        except:
+            temp = set()
+            temp.add(course[0:6])
+            done['2101'] = temp
+    else:
+        try:
+            done[course[0:4]].add(course[5:11])
+        except:
+            temp = set()
+            temp.add(course[5:11])
+            done[course[0:4]] = temp
 
-for i in range(0, 6):
-    for j in range(i+1, 6):
-        set_fail = set_fail.union(s[i].intersection(s[j]))
-    
-print("\nFailed in:")
-for i in set_fail:
-    print(i)
+temp = set()
+failed = set()
+
+done = dict(sorted(done.items()))
+
+for key,value in done.items():
+    print(f"\n{key}")
+    for val in value:
+        print(val)
+        if val in temp:
+            failed.add(val)
+        temp.add(val)
+
+
+if len(failed) > 0:
+    print("\nFailed")
+    for fail in failed:
+        print(fail)
